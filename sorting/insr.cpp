8a2120a7b421846b8312c12dd2ff0436d4116838
Your Name <your.email@example.com>
2025-09-28 10:28:11 +0530

    bubble sort
    git push --force-with-lease
    git push --force-with-lease

--- sorting/insr.cpp
+++ sorting/insr.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -28,3 +29,35 @@ int main() {
    }
     
 }
+=======
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the length of array: ";
+    cin >> n;
+
+    vector<int> arr(n); // safe, resizable, standard C++
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout<<"Applying selection sort->";
+   for(int i=0;i<=n-2;i++){
+    int mini=i;
+    for(int j=i;j<n;j++){
+        if(arr[j]<arr[mini]){
+            mini=j;
+        }
+    }
+    int temp=arr[mini];
+    arr[mini]=arr[i];
+    arr[i]=temp;
+   }
+   for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+   }
+    
+}
+>>>>>>> f2ebfc8 (bubble sort)
